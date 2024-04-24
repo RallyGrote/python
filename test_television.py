8eef24da0ba13692a352d38558c6220446034a34
RallyGrote <rallygrote@gmail.com>
2024-04-23 22:19:29 -0500

    Added docstrings and type hinting

--- test_television.py
+++ test_television.py
@@ -1,17 +1,26 @@
 import pytest
 from television import Television
 
-def test_init():
+def test_init() -> None:
+    """
+    tests the __init__ function in the television class
+    """
     test_tv = Television()
     assert test_tv.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
-def test_power():
+def test_power() -> None:
+    """
+    tests the power function in the television class
+    """
     test_tv = Television()
     assert test_tv.__str__() == 'Power = False, Channel = 0, Volume = 0'
     test_tv.power()
     assert test_tv.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
-def test_mute():
+def test_mute() -> None:
+    """
+    tests the mute function in the television class
+    """
     test_tv = Television()
     test_tv.power()
     test_tv.volume_up()
@@ -25,7 +34,10 @@ def test_mute():
     test_tv.mute()
     assert test_tv.__str__() == 'Power = False, Channel = 0, Volume = 2'
 
-def test_channel_up():
+def test_channel_up() -> None:
+    """
+    tests the channel_up function in the television class
+    """
     test_tv = Television()
     test_tv.channel_up()
     assert test_tv.__str__() == 'Power = False, Channel = 0, Volume = 0'
@@ -37,7 +49,10 @@ def test_channel_up():
     test_tv.channel_up()
     assert test_tv.__str__() == 'Power = True, Channel = 0, Volume = 0'
 
-def test_channel_down():
+def test_channel_down() -> None:
+    """
+    tests the channel_down function in the television class
+    """
     test_tv = Television()
     test_tv.channel_down()
     assert test_tv.__str__() == 'Power = False, Channel = 0, Volume = 0'
@@ -45,7 +60,10 @@ def test_channel_down():
     test_tv.channel_down()
     assert test_tv.__str__() == 'Power = True, Channel = 3, Volume = 0'
 
-def test_volume_up():
+def test_volume_up() -> None:
+    """
+    tests the volume_up function in the television class
+    """
     test_tv = Television()
     test_tv.volume_up()
     assert test_tv.__str__() == 'Power = False, Channel = 0, Volume = 0'
@@ -59,7 +77,10 @@ def test_volume_up():
     test_tv.volume_up()
     assert test_tv.__str__() == 'Power = True, Channel = 0, Volume = 2'
 
-def test_volume_down():
+def test_volume_down() -> None:
+    """
+    tests the volume_down function in the television class
+    """
     test_tv = Television()
     test_tv.volume_up()
     test_tv.volume_up()
