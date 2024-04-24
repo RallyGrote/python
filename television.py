554309c947b8a2437f75050cf5aaa05842be8420
RallyGrote <rallygrote@gmail.com>
2024-04-23 21:02:23 -0500

    Added docstrings

--- television.py
+++ television.py
@@ -4,6 +4,9 @@ class Television:
     MIN_CHANNEL = 0
     MAX_CHANNEL = 3
     def __init__(self):
+        """
+        Method to set default values of television object.
+        """
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
@@ -11,12 +14,19 @@ class Television:
         self.__mute_volume = Television.MIN_VOLUME
 
     def power(self):
+        """
+        Turns the television on/off
+        """
         if self.__status:
             self.__status = False
         else:
             self.__status = True
 
     def mute(self):
+        """
+        Saves the volume then sets it to 0 if muted is false
+        otherwise sets volume to the previously saved volume
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -29,6 +39,10 @@ class Television:
             pass
 
     def channel_up(self):
+        """
+        Sets the channel up one value. If called while at the maximum
+        channel, sets the channel to the minimum channel
+        """
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
@@ -37,6 +51,10 @@ class Television:
         else:
             pass
     def channel_down(self):
+        """
+        Sets the channel down one value. If called while at the minimum
+        channel, sets the channel to the maximum channel
+        """
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
@@ -46,6 +64,10 @@ class Television:
             pass
 
     def volume_up(self):
+        """
+        Sets the volume up one value. If volume is at the maximum
+        volume, then the volume is unaffected
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -58,6 +80,10 @@ class Television:
             pass
 
     def volume_down(self):
+        """
+        Sets the volume down one value. If volume is at the minimum
+        volume, then the volume is unaffected
+        """
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -70,6 +96,9 @@ class Television:
             pass
 
     def __str__(self):
+        """
+        Sets the layout in which a television object will be printed
+        """
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
 
 
